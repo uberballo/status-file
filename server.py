ce801f6f759f64deacc3a730f3a84d1ebf8869cd
uberballo <kasper.paavolainen@hotmail.com>
2019-11-14 18:20:55 +0200

    Added errror message and refactoring

--- server.py
+++ server.py
@@ -1,4 +1,5 @@
 import os
+import sys
 from flask import Flask, request, render_template, url_for, redirect
 from werkzeug.utils import secure_filename
 from packageParser import parsePackages, findDependants
@@ -10,9 +11,11 @@ packages  = []
 statusPath = ""
 #findDependants(packages)
 
-@app.route("/", methods=['GET', 'POST'])
-def fileFrontPage():
-    return render_template('frontpage.html')
+@app.route("/" )
+def fileFrontPage(errorMessage = ""):
+    global packages
+    packages = []
+    return render_template('frontpage.html',errorMessage= errorMessage)
 
 @app.route("/packages")
 def allPackages():
@@ -20,32 +23,33 @@ def allPackages():
     if not packages:
         packages = parsePackages("status")
         findDependants(packages)
-    return render_template('layout.html', packages = packages)
+    return render_template('index.html', packages = packages)
 
 @app.route("/packages/<packageName>")
 def singlePackage(packageName):
     shownPackage = next(package for package in packages if package.name ==
                         packageName)
-    #print(shownPackage.name)
-    #print(shownPackage.descriptionBeginning)
-    #print(shownPackage.dependencies)
     return render_template('package.html', package=shownPackage)
 
 @app.route("/handleUpload", methods=['POST'])
 def handleFileUpload():
     if 'statusFile' in request.files:
         statusFile = request.files['statusFile']
-        if statusFile.filename == 'status' or statusFile.filename =='status.real':
-            fileName = secure_filename(statusFile.filename)
-            path = os.path.join(app.instance_path, 'status', fileName)
-            statusFile.save(path)
+        try:
+            if statusFile.filename == 'status' or statusFile.filename =='status.real':
+                fileName = secure_filename(statusFile.filename)
+                path = os.path.join(app.instance_path, 'status', fileName)
+                statusFile.save(path)
 
-            global packages
-            packages = parsePackages(path)
-            findDependants(packages)
+                global packages
+                packages = parsePackages(path)
+                findDependants(packages)
 
-            return render_template('layout.html', packages = packages)
-    return render_template('frontpage.html')
+                return render_template('index.html', packages = packages)
+        except:
+            #errorMessage = "Error "+sys.exc_info()[0]+" occured"
+            errorMessage = "Error {} occured".format(sys.exc_info()[0])
+            return render_template('frontpage.html', errorMessage = errorMessage)
 
 if __name__ == "__main__":
     app.run()
